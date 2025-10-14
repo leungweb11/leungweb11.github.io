7f7fc4fed7aa3d169c8ed8929ecc54cac191e26e
EKHH <90589480+EKHH@users.noreply.github.com>
2025-10-14 09:40:41 -0700

    Remove debug command.

--- e2/scripts/helper.js
+++ e2/scripts/helper.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
 	const current_page = location.pathname.split("/")[2];
 	if (current_page === "") return;
 	const navLinks = document.querySelectorAll("nav ul li a");
-	console.log(document.querySelectorAll("nav ul li a"))
 	for (const navLink of navLinks) {
 		if (navLink.getAttribute("href") == current_page) {
 			navLink.parentElement.className += "nav-active";
