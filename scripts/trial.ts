3f6764d2da1e74b809fa6490caff99d808dd7731
EKHH <90589480+EKHH@users.noreply.github.com>
2025-11-30 16:31:51 -0800

    Update format.

--- scripts/trial.ts
+++ scripts/trial.ts
@@ -1,30 +1,40 @@
 import { waapi, animate, svg, splitText, stagger } from "animejs";
 // Animate fill parameters.
-waapi.animate(("path, rect, ellipse, polygon, circle"), {
+waapi.animate("path, rect, ellipse, polygon, circle", {
 	fill: { from: "rgba(255, 255, 255, 0)" },
 	ease: "inOutQuad",
-	duration: 2000
+	duration: 2000,
 });
 
 // Use pure black to mark paths to be drawn then erased.
-animate(svg.createDrawable("path[stroke='#000'], rect[stroke='#000'], ellipse[stroke='#000'], polygon[stroke='#000'], circle[stroke='#000']"), {
-	draw: ["0 0", "0 1", "1 1"], // Reversed order to ensure simultaneous effect with colour filling.
-	ease: "inOutQuad",
-	duration: 2000
-});
+animate(
+	svg.createDrawable(
+		"path[stroke='#000'], rect[stroke='#000'], ellipse[stroke='#000'], polygon[stroke='#000'], circle[stroke='#000']",
+	),
+	{
+		draw: ["0 0", "0 1", "1 1"], // Reversed order to ensure simultaneous effect with colour filling.
+		ease: "inOutQuad",
+		duration: 2000,
+	},
+);
 // Draw paths with colour #333.
-animate(svg.createDrawable("path[stroke='#333'], rect[stroke='#333'], ellipse[stroke='#333'], polygon[stroke='#333'], circle[stroke='#333']"), {
-	draw: ["0 0", "0 1"],
-	ease: "inOutQuad",
-	duration: 1000
-});
+animate(
+	svg.createDrawable(
+		"path[stroke='#333'], rect[stroke='#333'], ellipse[stroke='#333'], polygon[stroke='#333'], circle[stroke='#333']",
+	),
+	{
+		draw: ["0 0", "0 1"],
+		ease: "inOutQuad",
+		duration: 1000,
+	},
+);
 
 const { chars } = splitText("tspan", { chars: true });
-console.info(chars)
+console.info(chars);
 waapi.animate("tspan", {
 	translate: `0 -2rem`,
 	fill: { from: "rgba(255, 255, 255, 0)" },
 	ease: "inOutQuad",
 	delay: stagger(100),
-	duration: 2000
+	duration: 2000,
 });
