c91ceccc64f0590c51bd759daa7e12c5d2912b59
EKHH <90589480+EKHH@users.noreply.github.com>
2025-12-19 19:09:56 -0800

    Removed animation when user prefers-reduced-motion.

--- scripts/animate.ts
+++ scripts/animate.ts
@@ -1,34 +1,45 @@
-import { animate, engine, svg, utils } from "animejs";
+import { animate, engine, svg, utils, JSAnimation } from "animejs";
+const no_animation = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
 
-const animation = [
-	// Draw paths with colour #333.
-	animate(svg.createDrawable("path[stroke='#333'], rect[stroke='#333'], ellipse[stroke='#333'], polygon[stroke='#333'], circle[stroke='#333']"), {
-		draw: ["0 0", "0 1"],
-		ease: "inOutQuad",
-		duration: 1000,
-	}),
-	// Fade text elements.
-	animate("text", {
-		opacity: [0, 1],
-		delay: 1000,
-		duration: 1000,
-		ease: "inOutQuad",
-	}),
-	// Use pure black to mark paths to be drawn then erased.
+let animation: JSAnimation[] = [
 	animate(svg.createDrawable("path[stroke='#000'], rect[stroke='#000'], ellipse[stroke='#000'], polygon[stroke='#000'], circle[stroke='#000']"), {
-		draw: ["0 0", "0 1", "1 1"], // Reversed order to ensure simultaneous effect with colour filling.
-		ease: "inOutQuad",
-		duration: 2000,
-	}),
-	// Animate fill parameters.
-	animate("path, rect, ellipse, polygon, circle", {
-		fill: { from: "rgba(255, 255, 255, 0)" },
-		ease: "inOutQuad",
-		duration: 2000,
+		draw: ["0 0"], // Instantly hides the drawing outline; invoked only if animation is off.
+		duration: 0,
 	}),
 ];
 
+if (!no_animation) {
+	animation = [
+		// Draw paths with colour #333.
+		animate(svg.createDrawable("path[stroke='#333'], rect[stroke='#333'], ellipse[stroke='#333'], polygon[stroke='#333'], circle[stroke='#333']"), {
+			draw: ["0 0", "0 1"],
+			ease: "inOutQuad",
+			duration: 1000,
+		}),
+		// Fade text elements.
+		animate("text", {
+			opacity: [0, 1],
+			delay: 1000,
+			duration: 1000,
+			ease: "inOutQuad",
+		}),
+		// Use pure black to mark paths to be drawn then erased.
+		animate(svg.createDrawable("path[stroke='#000'], rect[stroke='#000'], ellipse[stroke='#000'], polygon[stroke='#000'], circle[stroke='#000']"), {
+			draw: ["0 0", "0 1", "1 1"], // Reversed order to ensure simultaneous effect with colour filling.
+			ease: "inOutQuad",
+			duration: 2000,
+		}),
+		// Animate fill parameters.
+		animate("path, rect, ellipse, polygon, circle", {
+			fill: { from: "rgba(255, 255, 255, 0)" },
+			ease: "inOutQuad",
+			duration: 2000,
+		}),
+	];
+}
+
 export async function eraseArt() {
+	if (no_animation) return;
 	utils.sync(() => (engine.speed = 1.5));
 	let finished = animation[0]?.reverse();
 	for (let i = 1; i < animation.length; i++) {
@@ -40,6 +51,7 @@ export async function eraseArt() {
 }
 
 export function refreshAnimation() {
+	if (no_animation) return;
 	for (let i = 0; i < animation.length; i++) {
 		animation[i]?.refresh().restart();
 	}
