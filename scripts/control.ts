42917ce4f2bf4c4e03bd553bbb98fb6101f09d02
EKHH <90589480+EKHH@users.noreply.github.com>
2025-12-19 19:10:42 -0800

    Refactored messy code.

--- scripts/control.ts
+++ scripts/control.ts
@@ -1,65 +1,75 @@
 import { eraseArt, refreshAnimation } from "./animate";
 
 const svg = document.getElementsByTagName("svg");
-const no_animate = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
 const control_form = <HTMLFormElement>document.getElementById("control");
-let pg_display = <HTMLInputElement>document.getElementById("pg_display");
-let pg_cached = parseInt(pg_display.value) - 1 || 0;
+// The first button is always triggered on enter. Since changes of the input value are already being detected and handled, the said button was wrapped in the label element with no function.
+const down_button = <HTMLButtonElement>control_form.children[1];
+let pg_display = <HTMLInputElement>control_form.children[2];
+const up_button = <HTMLButtonElement>control_form.children[3];
+
+function invalid() {
+	return pg_display.matches(":invalid"); // Use HTML 5 form validation.
+}
+
+// Page numeral starts from 1 whereas page index starts from 0.
+async function updatePage() {
+	let pg_index = parseInt(pg_display.value) - 1;
+	// Always update the state of navigation buttons.
+	if (svg.length <= pg_index + 1) {
+		up_button?.setAttribute("disabled", "disabled");
+	} else {
+		up_button?.removeAttribute("disabled");
+	}
+	if (0 >= pg_index) {
+		down_button?.setAttribute("disabled", "disabled");
+	} else {
+		down_button?.removeAttribute("disabled");
+	}
+	// Input validation.
+	if (invalid()) return;
+	if (svg.length <= pg_index) {
+		console.error("Input exceeds total page numeral; navigating to the last page.");
+		pg_index = svg.length - 1;
+	} else if (pg_index < 0) {
+		console.warn("Input is invalid; navigating to the first page.");
+		pg_index = 0;
+	}
+	// Erase the board; used alongside refreshAnimation. Has no effect if prefers-reduced-motion was detected.
+	await eraseArt();
+	for (let i = 0; i < svg.length; i++) {
+		if (i === pg_index) continue; // Exception for the target page.
+		svg[i]?.setAttribute("display", "none"); // Hide everything else.
+	}
+	svg[pg_index]?.setAttribute("display", "inline");
+	refreshAnimation();
+}
 
 // First run.
-if (!pg_display) {
-	console.error("Could not find display element for tracking page numeral; creating hidden input.");
-	pg_display = document.createElement("input");
-	pg_display.setAttribute("display", "none");
-}
-
 pg_display.setAttribute("max", String(svg.length));
-
-function hideAll(exclude?: number) {
-	for (let i = 0; i < svg.length; i++) {
-		if (i !== exclude) {
-			svg[i]?.setAttribute("display", "none");
-		}
-	}
-}
-
-// Page numeral starts from zero.
-function updatePage(pg_input = 0) {
-	if (pg_display) {
-		if (svg.length <= pg_input) {
-			console.warn("Input exceeds total page numeral; navigating to the last page.");
-			pg_input = svg.length - 1; // Input too large; navigate to the last page.
-			pg_display.value = String(pg_input + 1);
-		} else if (svg.length - pg_input > svg.length) {
-			console.warn("Input is invalid; navigating to the first page.");
-			pg_input = 0;
-			pg_display.value = String(pg_input + 1);
-		}
-		hideAll(pg_input); // Exclude the target page.
-		if (svg.length > pg_input) {
-			svg[pg_input]?.setAttribute("display", "inline");
-		}
-	}
-}
-
 control_form.addEventListener("submit", function (event) {
 	event.preventDefault(); // Do not allow submitting the form; it causes a refresh that is undesirable.
 });
-
-// Update page on page change.
-pg_display.addEventListener("change", async () => {
-	if (pg_display.matches(":invalid")) {
-		return; // Return if input is invalid.
-	}
-	if (!no_animate) {
-		await eraseArt();
-	}
-	pg_cached = parseInt(pg_display.value) - 1 || 0; // Upddate cache.
-	updatePage(pg_cached);
-	refreshAnimation();
-});
-
-hideAll();
-updatePage(pg_cached); // Browser may retain user input after page refresh. If possible, restore to previous state.
+pg_display.addEventListener("change", () => updatePage()); // Detect and apply user changes.
+if (control_form.length >= 3) {
+	down_button?.addEventListener("click", function () {
+		if (pg_display.disabled) return; // Already at minimum.
+		if (invalid()) {
+			pg_display.value = String(svg.length); // Redirect user to the nearest valid value.
+		} else {
+			pg_display.value = String(parseInt(pg_display.value) - 1); // Navigate to the previous page.
+		}
+		updatePage();
+	});
+	up_button?.addEventListener("click", function () {
+		if (pg_display.disabled) return;
+		if (invalid()) {
+			pg_display.value = "1";
+		} else {
+			pg_display.value = String(parseInt(pg_display.value) + 1);
+		}
+		updatePage();
+	});
+}
+updatePage();
 
 export { pg_display };
